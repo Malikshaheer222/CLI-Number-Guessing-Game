94bd74e4bc99c94be3cec8b878e06b0ea087367a
Shaheer Malik <139039230+Malikshaheer222@users.noreply.github.com>
2023-09-04 06:06:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let ans = Math.floor(Math.random() * 10);
+const answers = await inquirer.prompt([
+    {
+        name: `UserGuess`,
+        type: `number`,
+        message: `Guess the number`
+    },
+]);
+if (answers.UserGuess === ans) {
+    console.log(chalk.bgBlueBright `Hurrah! correct guess`);
+}
+else {
+    console.log(chalk.red `Better luck next time`);
+}
+console.log(`Correct Answer:${ans}`);
